1a8f74585cfdfec4fceca7479cf6f6d7dd90f850
Tomasz Szatkowski <sharp.coding@gmail.com>
2017-03-07 17:00:26 +0100

    function prop is created once,  xScale renamed to getXScale

--- client/scatterPlotWidget/index.tsx
+++ client/scatterPlotWidget/index.tsx
@@ -16,27 +16,29 @@ export class ScatterPlot extends React.Component<ScatterPlotProps, void> {
   xMax   = (data)  => d3.max(data, (d: Point2D) => d.x);
   yMax   = (data)  => d3.max(data, (d: Point2D) => d.y);
   
-  xScale = (props: ScatterPlotProps) => {
+  getXScale = (props: ScatterPlotProps) => {
     return d3.scale.linear()
       .domain([0, this.xMax(props.data)])
       .range([props.padding, props.width - props.padding * 2]);
   };
   
-  yScale = (props: ScatterPlotProps) => {
+  getYScale = (props: ScatterPlotProps) => {
     return d3.scale.linear()
       .domain([0, this.yMax(props.data)])
       .range([props.height - props.padding, props.padding]);
   };
 
   render() {
+    var xScale = this.getXScale(this.props);
+    var yScale = this.getYScale(this.props);
     return (
       <svg width={this.props.width} height={this.props.height}>
-        <DataCircles data={this.props.data} xScale={this.xScale(this.props)} yScale={this.xScale(this.props)} />
+        <DataCircles data={this.props.data} xScale={xScale} yScale={yScale} />
         <XYAxis 
           height={this.props.height} 
-          padding={30}
-          xScale={this.xScale(this.props)} 
-          yScale={this.yScale(this.props)} />
+          padding={this.props.padding}
+          xScale={xScale} 
+          yScale={yScale} />
       </svg>
     );
   }
