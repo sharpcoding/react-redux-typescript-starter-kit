ecb86401d99228ddeb5098a6af1a1f32b61cac5f
Tomasz Szatkowski <sharp.coding@gmail.com>
2017-03-07 16:57:29 +0100

    fixing a typo

--- client/main.tsx
+++ client/main.tsx
@@ -5,13 +5,13 @@ import { Provider } from 'react-redux';
 import GraphMainScreen from './graphScreen';
 import graphScreenState from './graphScreen/reducer';
 
-const combinedRedures = combineReducers({
+const combinedReducers = combineReducers({
   graphScreenState
 });
 
 //this is the callback function required in order to have this Chrome extension https://github.com/zalmoxisus/redux-devtools-extension working
 const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
-const store: Store<any> = createStore(combinedRedures, composeEnhancers(applyMiddleware()));
+const store: Store<any> = createStore(combinedReducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
