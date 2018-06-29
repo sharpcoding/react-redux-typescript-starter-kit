bcfd583fb3a71ed4238884cf0ef2b8101074b144
sharp.coding <sharp.coding@gmail.com>
2018-06-29 15:11:18 +0200

    simplifying exports in action-creators

--- src/screens/bubble-chart-screen/action-creators.ts
+++ src/screens/bubble-chart-screen/action-creators.ts
@@ -1,18 +1,11 @@
 import * as _ from 'lodash';
 import { SetHeightAction, SetWidthAction } from './actions';
 
-type ISetWidthActionCreator = (width: string) => SetWidthAction;
-type ISetHeightActionCreator = (width: string) => SetHeightAction;
+export type ISetWidthActionCreator = (width: string) => SetWidthAction;
+export type ISetHeightActionCreator = (width: string) => SetHeightAction;
 
-const setWidth: ISetWidthActionCreator = (width: string) =>
+export const setWidth: ISetWidthActionCreator = (width: string) =>
   _.toPlainObject(new SetWidthAction(_.parseInt(width)));
 
-const setHeight: ISetHeightActionCreator = (height: string) =>
+export const setHeight: ISetHeightActionCreator = (height: string) =>
   _.toPlainObject(new SetHeightAction(_.parseInt(height)));
-
-export {
-  setWidth,
-  ISetWidthActionCreator,
-  setHeight,
-  ISetHeightActionCreator,
-};
